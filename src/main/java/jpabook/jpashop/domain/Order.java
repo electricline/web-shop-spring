b5a9dfaa3718a8924fec325db8b706ccf0f724ba
electricline <jktmobile6637@gmail.com>
2021-01-23 13:35:43 +0900

    update - eager(즉시 로딩) -> lazy(지연 로딩)으로 변경

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,6 +1,5 @@
 package jpabook.jpashop.domain;
 
-import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -10,21 +9,21 @@ import java.util.List;
 
 @Entity
 @Table(name = "orders")
-@Getter @Setter
+@Setter
 public class Order {
 
     @Id @GeneratedValue
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -33,4 +32,27 @@ public class Order {
     private OrderStatus status; // 주문상태 ORDER, CANCEL
 
 
+    public Long getId() {
+        return this.id;
+    }
+
+    public Member getMember() {
+        return this.member;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return this.orderItems;
+    }
+
+    public Delivery getDelivery() {
+        return this.delivery;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return this.orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return this.status;
+    }
 }
